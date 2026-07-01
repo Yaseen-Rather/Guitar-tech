a10965a864e1537fdb67bca3fee284575dd48e8e
Yaseen <yaseenbinashraf469@gmail.com>
2026-07-01 09:44:20 +0530

    new phase

--- convert.ps1
+++
@@ -1,3 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$image = [System.Drawing.Image]::FromFile("src\assets\app_icon.jpg")
-$image.Save("src\assets\temp_icon.png", [System.Drawing.Imaging.ImageFormat]::Png)
